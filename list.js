879d46667f24a40ef851a95276ca3ef73a6ce7a9
lucas mori <morilucasaluno@gmail.com>
2025-01-20 08:20:24 -0300

    fix: sort folders and file names

---
+++ list.js
@@ -0,0 +1,49 @@
+// 01 - script that returns "Hello World"
+const helloWorld = () => {
+    //enter your code
+}
+
+// 02 - script that returns sum of two numbers
+const sumTwoNumbers = () => {
+    //enter your code
+}
+
+// 03 - script that returns type of variable is number or not
+const checkTypeOfVariableIsNumber = () => {
+    //enter your code
+}
+
+// 04 - script taht returns type of variable is string or not
+const checkTypeofVariableIsString = () => {
+    //enter your code
+}
+
+//05 - script that returns type of variable is boolean or not
+const checkTypeOfVariableIsBoolean = () => {
+    //enter your code
+}
+
+// 06 - script that returns subtraction of two numbers
+const subtractionTwoNumbers = () => {
+    //enter your code
+}
+
+// 07 - script that returns multiplication of two numbers
+const multiplicationTwoNumbers = () => {
+    //enter your code
+}
+
+// 08 - script that returns division of two numbers
+const divisionTwoNumbers = () => {
+    //enter your code
+}
+
+// 09 - script that returns check number is even
+const checkNumberIsEvenOrOdd = () => {
+    //enter your code
+}
+
+// 10 - script that returns check number is odd
+const checkNumberIsOddOrEven = () => {
+    //enter your code
+}
